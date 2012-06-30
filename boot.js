2ed7a5450fb30e3a4dbff23bd0b711ddc08cafec
Hadrien Jouet <hadrien.jouet@grownseed.net>
2012-06-29 20:12:04 -0700

    Basic app skeleton

---
+++ boot.js
@@ -0,0 +1,22 @@
+/**
+ * Module dependencies.
+ */
+
+var vm = require('vm'),
+	fs = require('fs');
+
+module.exports = function(app, config)
+{
+	var dir = __dirname + '/routes';
+
+	fs.readdirSync(dir).forEach(function(file)
+	{
+		var str = fs.readFileSync(dir + '/' + file, 'utf8');
+
+		var context = { app: app, config: config };
+
+		for (var key in global) context[key] = global[key];
+
+		vm.runInNewContext(str, context, file);
+	});
+};
